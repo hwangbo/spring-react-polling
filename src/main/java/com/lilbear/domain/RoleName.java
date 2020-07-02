7e2a38c75f77f3990dbe3156046e09e72c915e9a
hwangbo <taekwonod714@naver.com>
2020-07-03 04:09:36 +0900

    feat: 도메인 구현

--- src/main/java/com/lilbear/domain/RoleName.java
+++ src/main/java/com/lilbear/domain/RoleName.java
@@ -1,4 +1,6 @@
 package com.lilbear.domain;
 
 public enum RoleName {
+    ROLE_USER,
+    ROLE_ADMIN
 }
--- src/main/java/com/lilbear/domain/RoleRepository.java
+++ src/main/java/com/lilbear/domain/RoleRepository.java
@@ -1,4 +1,9 @@
 package com.lilbear.domain;
 
-public interface RoleRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
 }
