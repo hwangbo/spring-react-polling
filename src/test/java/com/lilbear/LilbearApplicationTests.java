f3fc2afa012ea4df0add58dfd65b28fe6d54e46d
hwangbo <taekwonod714@naver.com>
2020-07-03 03:21:32 +0900

    feat: Java 8 날짜/시간 변환기 및 UTC 사용

---
+++ src/test/java/com/lilbear/LilbearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lilbear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LilbearApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
