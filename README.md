0886e3a725a002bb26510e2520d4a4b6e9ac2aff
hwangbo <taekwonod714@naver.com>
2020-07-04 21:15:16 +0900

    refactor: 디렉토리 변경, fix: application.properties 수정

--- README.md
+++
@@ -1,3 +0,0 @@
-# Spring Boot, React, JPA를 이용한 투표 앱
-
-[레퍼런스](https://woolbro.tistory.com/44)
--- src/main/java/com/lilbear/domain/role/RoleName.java
+++
@@ -1,6 +0,0 @@
-package com.lilbear.domain.role;
-
-public enum RoleName {
-    ROLE_USER,
-    ROLE_ADMIN
-}
